5389309e087aeb7c2b3d87080f070251ebb7611d
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-01 18:00:07 +0200

    Add files via upload

---
+++ mon_python_16/comanda.py
@@ -0,0 +1,7 @@
+def sum(a=1, b=2):
+	s = a + b
+	return s
+
+z = sum(a=5, b=6)
+print(z)
+print(sum())
--- mon_python_16/def_turtle.py
+++ mon_python_16/def_turtle.py
@@ -3,19 +3,17 @@ import turtle
 t=turtle.Pen()
 colors=['#CBC10B', '#0BCB49', '#0B41CB']
 
-i = 120 
-t.pencolor(colors[0])	# меняет цвет
-t.forward(100)			# рисует вперед
-t.left(i)				# поворачивает налево
 
-t.pencolor(colors[0])	# меняет цвет
-t.forward(100)			# рисует вперед
-t.left(i)				# поворачивает налево
+def side(i):
+	t.pencolor(colors[0])	# меняет цвет
+	t.forward(100)			# рисует вперед
+	t.left(i)				# поворачивает налево
 
-t.pencolor(colors[0])	# меняет цвет
-t.forward(100)			# рисует вперед
-t.left(i)				# поворачивает налево
 
+x = 120
+side(x)
+side(x)
+side(x)
 
 
 # 
