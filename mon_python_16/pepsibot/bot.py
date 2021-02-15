17f32fddcdced4874bd664fdaba9724f0ddeb3bb
pylinx64 <77532340+pylinx64@users.noreply.github.com>
2021-02-15 20:01:28 +0200

    Add files via upload

--- mon_python_16/pepsibot/bot.py
+++ mon_python_16/pepsibot/bot.py
@@ -6,6 +6,15 @@ bot = telebot.TeleBot('1615084228:AAH7ZYVsJMNhv15_6HL1yq2-B8TGZL3VRbs') 	# сю�
 def message_hello(message):
 	'''Отвечает на приветсвие человека'''
 	bot.send_message(message.chat.id, 'Ну привет!')
+	#print(message.chat.first_name, message.text)
+
+@bot.message_handler(content_types=['text'])
+def message_text(message):
+	'''Принимает текст и отвечает на него'''
+	# бот отправляет сообщение 
+	bot.send_message(message.chat.id, 'Как поживаешь?')
+	bot.send_message(message.chat.id, '🦶')
 
 
+print('#run bot...')
 bot.polling()
